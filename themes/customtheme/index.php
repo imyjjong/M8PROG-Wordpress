16766cc3aeb42727284f962a14de1fdead0f30ad
Vinnie <34916@ma-web.nl>
2024-06-20 22:35:29 +0200

    posts aangemaakt en te zien op posts page

--- themes/customtheme/index.php
+++ themes/customtheme/index.php
@@ -1,13 +1,18 @@
 <?php get_header(); ?>
-<main class="container">
-    <h1 class="mt-5">i believe in miraclesn wejosdkjigbu</h1>
-    <div class="content">
-        <p class="intro">
-            Intro
-        </p>
-        <p>
-            Content
-        </p>
-    </div>
+
+<main class="container my-5">
+    <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <h1 class="mt-5"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                <div class="col-lg-8 px-0">
+                    <?php the_excerpt(); ?>
+                </div>
+            </article>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <p><?php esc_html_e( 'bestaat niet eweboipjo' ); ?></p>
+    <?php endif; ?>
 </main>
+
 <?php get_footer(); ?>
