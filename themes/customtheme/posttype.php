eb3cd933a37680188e697887694693f3cf8a3493
Vinnie <34916@ma-web.nl>
2024-06-23 18:02:22 +0200

    taxonomy

--- themes/customtheme/posttype.php
+++ themes/customtheme/posttype.php
@@ -30,3 +30,62 @@
     ];
 
     register_post_type( 'yuh', $args );
+
+    $labels = [
+        'name'               => __( 'Projects', 'Text_Domain' ),
+        'singular_name'      => __( 'Project', 'Text_Domain' ),
+        'add_new'            => __( 'New Project', 'Text_Domain' ),
+        'add_new_item'       => __( 'Add New Project', 'Text_Domain' ),
+        'edit_item'          => __( 'Edit Project', 'Text_Domain' ),
+        'new_item'           => __( 'New Project', 'Text_Domain' ),
+        'view_item'          => __( 'View Project', 'Text_Domain' ),
+        'search_items'       => __( 'Search Project', 'Text_Domain' ),
+        'not_found'          => __( 'No Project Found', 'Text_Domain' ),
+        'not_found_in_trash' => __( 'No Project found in Trash', 'Text_Domain' ),
+    ];
+
+    $args = [
+        'labels'       => $labels,
+        'has_archive'  => true,
+        'public'       => true,
+        'hierarchical' => false,
+        'supports'     => [
+            'title',
+            'editor',
+            'excerpt',
+            'custom-fields',
+            'thumbnail',
+            'page-attributes',
+        ],
+        'rewrite'      => [ 'slug' => 'project' ],
+        'show_in_rest' => true,
+    ];
+
+    register_post_type( 'yuh', $args );
+
+    $labels = [
+        'name'              => _x( 'Origins', 'taxonomy general name', 'Text_Domain' ),
+        'singular_name'     => _x( 'Origin', 'taxonomy singular name', 'Text_Domain' ),
+        'search_items'      => __( 'Search Origins', 'Text_Domain' ),
+        'popular_items'     => __( 'Popular Origins', 'Text_Domain' ),
+        'all_items'         => __( 'All Origins', 'Text_Domain' ),
+        'parent_item'       => __( 'Parent Origin', 'Text_Domain' ),
+        'parent_item_colon' => __( 'Parent Origin:', 'Text_Domain' ),
+        'edit_item'         => __( 'Edit Origin', 'Text_Domain' ),
+        'update_item'       => __( 'Update Origin', 'Text_Domain' ),
+        'add_new_item'      => __( 'Add New Origin', 'Text_Domain' ),
+        'new_item_name'     => __( 'New Origin Name', 'Text_Domain' ),
+        'menu_name'         => __( 'Origins', 'Text_Domain' ),
+    ];
+
+    $args = [
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'show_in_rest'      => true,
+        'rewrite'           => [ 'slug' => 'origin' ],
+    ];
+
+    register_taxonomy( 'origin', 'project', 'yuh', $args );
