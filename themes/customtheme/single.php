9135907175fa120cd0218c1e74c706c6465ec769
Vinnie <34916@ma-web.nl>
2024-06-29 22:52:51 +0200

    styling single page, still looks bad but ugh whatever

--- themes/customtheme/single.php
+++ themes/customtheme/single.php
@@ -1,22 +1,63 @@
 <?php get_header(); ?>
 
-<main class="main">
+<main class="main main__project">
     <?php the_post(); ?>
     <section class="project">
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div class="project__content">
+            <?php
+                $content = get_the_content();
+
+                preg_match('/<img[^>]+>/', $content, $matches);
+
+                if (!empty($matches)) {
+                    echo $matches[0];
+                }
+                if ( has_post_thumbnail() ) {
+                    the_post_thumbnail();
+                }
+            ?>
             <h2 class="project__title"><?php the_title(); ?></h2>
+        </div>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <div class="project__information">
-                <p class="project__information--date"><?php the_date(); ?></p>
-                <p class="project__information--avatar"><?php echo get_avatar( get_the_author_meta('user_email'), '80', '-' ); ?></p>
-                <p><?php the_author_meta('description'); ?></p>
+                <p class="project__information--avatar"><?php echo get_avatar( get_the_author_meta( 'user_email' ), 80 ); ?></p>
+                <div class="project__information--wrapper">
+                    <p class="project__information--author">
+                        <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
+                            <?php echo get_the_author(); ?>
+                        </a>
+                    </p>
+                    <p class="project__information--date"><?php the_date(); ?></p>
+                    <p class="project__information--description"><?php the_author_meta( 'description' ); ?></p>
+                    <p class="project__information--category">
+                        <?php
+                        $categories = get_the_category();
+                        if ( ! empty( $categories ) ) {
+                            foreach ( $categories as $category ) {
+                                echo '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '">' . esc_html( $category->name ) . '</a> ';
+                            }
+                        }
+                        ?>
+                    </p>
+                </div>
             </div>
             <div class="project__content">
-                <?php the_content(); ?>
+                <?php
+                function filter_content_images( $content ) {
+                    return preg_replace( '/<img[^>]+\>/i', '', $content );
+                }
+                
+                $content = apply_filters( 'the_content', get_the_content() );
+                $content = filter_content_images( $content );
+                echo $content;
+                ?>
             </div>
         </article>
     </section>
 
-    <nav class="project-navigation">
+</main>
+
+<nav class="project-navigation">
         <?php
         the_post_navigation(
             [
@@ -26,6 +67,4 @@
         );
         ?>
     </nav>
-</main>
-
 <?php get_footer(); ?>
